ec815ec970ef07b70ab694935bccd63a2241f284
Kendra Wing <kendracwing@icloud.com>
2021-07-06 20:44:08 -0600

    Updated file

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const config = {
+	entry: "/public/index.js",
+	output: {
+		path: __dirname + "/dist",
+		filename: "bundle.js"
+	},
+	mode: "production",
+	plugins: [
+		new WebpackPwaManifest({
+			// the name of the generated manifest file
+			filename: "manifest.webmanifest",
+			inject: false,
+			fingerprints: false,
+			name: "Budget App",
+			short_name: "Budget App",
+			theme_color: "#ffffff",
+			background_color: "#ffffff",
+			start_url: "/",
+			display: "standalone",
+			icons: [
+				{
+					src: path.resolve(
+						__dirname,
+						"/icons/icon-512x512.png"
+					),
+					size: [72, 96, 128, 144, 152, 192, 384, 512]
+				}
+			]
+		})
+	]
+};
+
+module.exports = config;
